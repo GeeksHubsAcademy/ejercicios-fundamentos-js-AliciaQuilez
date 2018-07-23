de7512f17fd31ca2e09de1586564bf7e45c037fc
Alicia Quílez <aliciaquilezsayas@gmail.com>
2018-07-23 17:31:48 +0200

    ejercicios 2-objects completos

--- 2-objects/3-arrays.js
+++ 2-objects/3-arrays.js
@@ -1,4 +1,4 @@
-////////////////////////////// 
+//////////////////////////////
 //  3
 //  Crea un array en la variable llamada arr
 //  de tal modo que el siguiente código no muestre por consola 'suspendes'
@@ -6,75 +6,88 @@
 
 // Aquí tu código
 
+var arr = [{
+    name: 'pepito',
+    age: 25,
+  },
+  {
+    name: 'pepito',
+    age: 10
+  },
 
+  {
+    name: ['J','string'],
+    age: 5,
+  }
+];
 // Hasta aquí
 
 // TEST 1
 if (typeof arr === 'object' && arr.length >= 0) {
-    console.log('apruebas');
+  console.log('apruebas');
 } else {
-    console.log('suspendes');
+  console.log('suspendes');
 }
 // TEST 2
 if (typeof arr[0] === 'object') {
-    console.log('apruebas');
+  console.log('2- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('2- suspendes');
 }
 // TEST 3
 if (typeof arr[1] === 'object') {
-    console.log('apruebas');
+  console.log('3- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('3- suspendes');
 }
 // TEST 4
 if (typeof arr[2] === 'object') {
-    console.log('apruebas');
+  console.log('4- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('4- suspendes');
 }
 // TEST 5
 if (arr.length === 3) {
-    console.log('apruebas');
+  console.log('apruebas');
 } else {
-    console.log('suspendes');
+  console.log('suspendes');
 }
 // TEST 6
 if (arr[0].name === arr[1].name) {
-    console.log('apruebas');
+  console.log('6- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('6- suspendes');
 }
 
 // TEST 6
 if (arr[1].name === 'pepito') {
-    console.log('apruebas');
+  console.log('7- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('7- suspendes');
 }
 
 // TEST 7
 if (arr[1].age > arr[2].age) {
-    console.log('apruebas');
+  console.log('apruebas');
 } else {
-    console.log('suspendes');
+  console.log('suspendes');
 }
 // TEST 8
 if (arr[0].age === 25) {
-    console.log('apruebas');
+  console.log('9- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('9- suspendes');
 }
 // TEST 9
 if (typeof arr[2].name === 'string') {
-    console.log('apruebas');
+  console.log('10- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('10- suspendes');
 }
 
 // TEST 10
 if (arr[2].name[0] === 'J') {
-    console.log('apruebas');
+  console.log('11- apruebas');
 } else {
-    console.log('suspendes');
+  console.log('11- suspendes');
 }
