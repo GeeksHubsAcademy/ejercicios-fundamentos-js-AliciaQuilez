becea7d8efe1eb93625cc4f1d04969d077ff213b
Alicia Quílez <aliciaquilezsayas@gmail.com>
2018-08-08 17:51:51 +0200

    h

--- PROMISE-Ejercicios/program.js
+++ PROMISE-Ejercicios/program.js
@@ -1,15 +1,25 @@
-function parsePromised(json) {
-    try{
-       var promise = Promise.resolve(JSON.parse(json))
-    }catch(e){
-       var promise = Promise.reject(e);
-    }
-    return promise;
-  }
-  
-  function onReject(error) {
-    console.log(error.message);
-  }
-  
-  parsePromised(process.argv[2])
-  .then(null, onReject);
+
+function alwaysThrows(){
+    throw new Error('OH NOES!');
+}
+
+function iterate(num){
+    console.log(num);
+    return num + 1;
+}
+
+var promise = Promise.resolve(iterate(1))
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(null, onReject);
+
+function onReject(error){
+    console.log(error.message)
+}
